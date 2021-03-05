85893c4e7a73502fd3fc5a66827b2b0d8466a5bf
karthikairasu <karthikairasuk@gmail.com>
2021-03-05 22:54:14 +0530

    Create function2.js

---
+++ javascript/function2.js
@@ -0,0 +1,22 @@
+console.log("Function with required parameter");
+
+function one(a , b){
+    var c = a+b;
+    console.log("The function one say : "+ c);
+}
+
+
+one(100 , 200);
+one(100);
+
+
+// function with optional parameter
+
+function two(a=null , b=null){
+    var c = a+b;
+    console.log("The function one say : "+ c);
+}
+
+two(100 , 200);
+two(100);
+two();
