ee47df1cb678c01e4da5c03d449ba4ecbad860d7
Karthikairasu K <karthikairasuk@gmail.com>
2021-03-07 17:22:15 +0530

    Create forloop.js

---
+++ javascript/forloop.js
@@ -0,0 +1,12 @@
+console.log("The for loop Asc order example");
+
+for(var i=1; i<=20; i++)
+{
+    console.log("The Counter is : "+ i);
+}
+
+
+console.log("Descending order for loop");
+for(var i=20; i>=1; i--){
+    console.log("The Counter is :"+ i);
+}
