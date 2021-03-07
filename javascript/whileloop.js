028b42040e5878c5499f8141685092ebde083568
Karthikairasu K <karthikairasuk@gmail.com>
2021-03-07 18:13:42 +0530

    Create whileloop.js

---
+++ javascript/whileloop.js
@@ -0,0 +1,21 @@
+var counter = 1;
+while (counter <=10){
+    console.log(counter);
+    counter++;
+}
+
+console.log("------------------");
+var mycounter = 20;
+while(mycounter >=30){
+    console.log(mycounter);
+    mycounter--;
+}
+
+
+console.log("------Do While Desc Example-------");
+var counter = 20;
+do{
+    console.log("The Do While Say : "+ counter);
+    counter--;
+}
+while(counter > 30);
